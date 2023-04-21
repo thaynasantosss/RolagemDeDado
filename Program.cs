15451cac15517afa6369d38ba86743659a3be95a
thaynasantosss <thayna.2015.09@gmail.com>
2023-04-20 22:45:41 -0300

    Tarefa Concluída

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("--- Rolagem de Dado ***");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Quantas faces? ");
+int faces = Convert.ToInt32(Console.ReadLine());
+
+int rolagem = RandomNumberGenerator.GetInt32(1, faces + 1);
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine($"Rolagem: {rolagem}");
